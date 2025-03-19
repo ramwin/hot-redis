0fe0c415e6f00ee18a1502b9345e262aa9266cab
Xiang Wang <ramwin@qq.com>
2025-03-19 20:03:50 +0800

    feat: add wait

--- src/hot_redis/debounce_task.py
+++ src/hot_redis/debounce_task.py
@@ -27,7 +27,27 @@ class DebounceTask:
     def add_task(self, taskid: str):
         self.client.zadd(self.key, {taskid: self.get_time()}, nx=True)
 
-    def pop_tasks(self) -> List[str]:
+    def pop_tasks(self, max_wait: int=0) -> List[str]:
+        """
+        params:
+            max_wait: 1 means 0.1 seconds
+        """
+        tasks = self._pop_tasks()
+        if tasks or (not max_wait):
+            return tasks
+        first_task = self.client.zrange(self.key, 0, 0, withscores=True)
+        if not first_task:
+            time.sleep(max_wait * 0.1)
+            return []
+        add_time = first_task[0][1]
+        now = self.get_time()
+        need_wait = max(0, self.timeout - (now - add_time))
+        if need_wait <= max_wait:
+            time.sleep(need_wait / 10)
+            return self._pop_tasks()
+        return self._pop_tasks()
+
+    def _pop_tasks(self) -> List[str]:
         now = self.get_time()
         delete_before = now - self.timeout
 
