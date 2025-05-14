9b5dc10e2fd9d0d60ad183e2e4de0dde01334582
Xiang Wang <ramwin@qq.com>
2025-05-14 23:26:41 +0800

    feat: multi_wait_task support rediscluster

--- src/hot_redis/multi_wait_task.py
+++ src/hot_redis/multi_wait_task.py
@@ -3,12 +3,18 @@
 
 
 import json
-from redis import Redis
+import random
+
 from typing import (
         List,
         Optional,
         Tuple,
+        Union,
         )
+
+from redis import Redis
+from redis.cluster import RedisCluster
+
 
 
 class MultiWaitTask:
@@ -16,19 +22,34 @@ class MultiWaitTask:
     wait multi keys for handler
     """
 
-    def __init__(self, client: Redis, keys: List[str], batch_size: int = 10):
+    def __init__(self, client: Union[Redis, RedisCluster], keys: List[str], batch_size: int = 10):
         self.client = client
         self.keys = keys
         self.batch_size = batch_size
 
     def get_tasks(self) -> Optional[Tuple[str, List[dict]]]:
-        result = self.client.blmpop(
-                10,
-                len(self.keys),
-                *self.keys,
-                direction="LEFT",
-                count=self.batch_size,
-        )
+        if isinstance(self.client, Redis):
+            result = self.client.blmpop(
+                    10,
+                    len(self.keys),
+                    *self.keys,
+                    direction="LEFT",
+                    count=self.batch_size,
+            )
+        else:
+            key_result = None
+            for key in random.sample(self.keys, len(self.keys)):
+                key_result = self.client.lpop(key, self.batch_size)
+                if not key_result:
+                    continue
+                else:
+                    break
+            if not key_result:
+                return None
+            return key, [
+                    json.loads(i)
+                    for i in key_result
+            ]
         if not result:
             return None
         return result[0], [
--- tests/test_multi_wait_task.py
+++ tests/test_multi_wait_task.py
@@ -5,6 +5,7 @@
 import logging
 import unittest
 from redis import Redis
+from redis.cluster import RedisCluster
 
 from hot_redis.multi_wait_task import MultiWaitTask
 
@@ -17,6 +18,20 @@ class Test(unittest.TestCase):
     def test1(self):
         task = MultiWaitTask(
                 client=Redis(decode_responses=True),
+                keys=["list1", "list2"], batch_size=3
+        )
+        task.add_task("list1", ["list1task_a"])
+        task.add_task("list1", ["list1task_b"])
+        task.add_task("list2", ["list2task"])
+        LOGGER.error(task.get_tasks())
+        LOGGER.error(task.get_tasks())
+        LOGGER.error(task.get_tasks())
+
+    def test2(self):
+        task = MultiWaitTask(
+                client=RedisCluster.from_url(
+                    "redis://localhost:7000",
+                    decode_responses=True),
                 keys=["list1", "list2"], batch_size=3
         )
         task.add_task("list1", ["list1task_a"])
